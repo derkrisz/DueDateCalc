0c5a64f2020a5cfbc17faa5e24b342f4850f88db
Krisztian Der <krisztian.der@gmail.com>
2021-05-04 22:18:26 +0100

    wip

--- due_date_calc.py
+++ due_date_calc.py
@@ -1,4 +1,5 @@
-days = ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY')
+days = ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY')
+hours = (1, 2, 3, 4, 5, 9, 10, 11, 12)
 
 def get_am_pm(input):
     split_input = input.split()
@@ -9,10 +10,10 @@ def get_am_pm(input):
 
 def get_time(input):
     split_input = input.split()
-    time = split_input[1].split(':')
-    if (1 <= int(time[0]) > 12):
-        raise ValueError('Hours should be between 1 and 12')
-    if (0 <= int(time[1]) > 59):
+    time = list(map(int, split_input[1].split(':')))
+    if (time[0] not in hours):
+        raise ValueError('Hours should fall between working hours: 9 AM to 5 PM')
+    if (0 <= time[1] > 59):
         raise ValueError('Minutes should be between 0 and 60')
     return time
 
@@ -22,6 +23,31 @@ def get_day(input):
     if (day_upper not in days):
         raise ValueError("Day should be a day of the week e.g. 'Monday', 'Tuesday'")
     return day_upper
+
+def calculate_turnaround_time(turnaround_time):
+    return turnaround_time / 12, turnaround_time % 12
+
+def calculate_due_date(date, turnaround_time):
+    
+    day = get_day(date)
+
+    time = get_time(date)
+
+    time_of_day = get_am_pm(date)
+
+    turnaround = calculate_turnaround_time(turnaround_time)
+
+    due_date_day = time_of_day[1] + turnaround[1]
+
+    
+
+
+
+    
+
+    
+
+
 
 
 
--- due_date_calc_test.py
+++ due_date_calc_test.py
@@ -11,7 +11,7 @@ class TestDueDateCalculator(unittest.TestCase):
 
     def test_get_time(self):
         time = due_date_calc.get_time(self.input)
-        output = ['4', '15']
+        output = [4, 15]
         self.assertListEqual(time, output)
         
     def test_get_day(self):
@@ -24,11 +24,17 @@ class TestDueDateCalculator(unittest.TestCase):
             result = due_date_calc.get_am_pm(input)
         self.assertEqual(str(e.exception),'Time of day should be AM or PM')
 
-    def test_invalid_hours_exception(self):
+    def test_invalid_hours_bad_format(self):
         invalid_hour = 'Sunday 14:50 pm'
         with self.assertRaises(ValueError) as e:
             result = due_date_calc.get_time(invalid_hour)
-        self.assertEqual(str(e.exception), 'Hours should be between 1 and 12')
+        self.assertEqual(str(e.exception), 'Hours should fall between working hours: 9 AM to 5 PM')
+
+    def test_invalid_hours_out_of_hours(self):
+        invalid_hour = 'Sunday 8:50 pm'
+        with self.assertRaises(ValueError) as e:
+            result = due_date_calc.get_time(invalid_hour)
+        self.assertEqual(str(e.exception), 'Hours should fall between working hours: 9 AM to 5 PM')
 
     def test_invalid_minutes_exception(self):
         invalid_minutes = 'Thursday 12:60 am'
@@ -41,6 +47,20 @@ class TestDueDateCalculator(unittest.TestCase):
         with self.assertRaises(ValueError) as e:
             result = due_date_calc.get_day(input)
         self.assertEqual(str(e.exception),"Day should be a day of the week e.g. 'Monday', 'Tuesday'")
+
+    def test_calculate_turnaround_time(self):
+        turnaround = due_date_calc.calculate_turnaround_time(6)
+        turnaround_2 = due_date_calc.calculate_turnaround_time(26)
+        self.assertEqual(turnaround, (0, 6))
+        self.assertEqual(turnaround_2, (2, 2))
+
+    def test_calculate_turnaround_time_over_a_day(self):
+        turnaround_2 = due_date_calc.calculate_turnaround_time(26)
+        self.assertEqual(turnaround_2, (2, 2))
+
+    def test_calculate_due_date_turnaround_at_current_day(self):
+        due_date = due_date_calc.calculate_due_date('Thursday 2:00 PM', 2)
+        self.assertEqual(due_date, 'Thursday 4:00 PM')   
 
 
 
