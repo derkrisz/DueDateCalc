623ebab352c740e4023079bbbb6ec8776579d7e6
Krisztian Der <krisztian.der@gmail.com>
2021-05-16 22:40:31 +0100

    Remove unnecessary brackets from ifs

--- due_date_calc.py
+++ due_date_calc.py
@@ -4,16 +4,16 @@ hours = (1, 2, 3, 4, 5, 9, 10, 11, 12)
 def get_am_pm(input):
     split_input = input.split()
     upper_am_pm = split_input[2]
-    if (upper_am_pm != 'PM' and upper_am_pm != 'AM'):
+    if upper_am_pm != 'PM' and upper_am_pm != 'AM':
         raise ValueError('Time of day should be AM or PM')
     return upper_am_pm
 
 def get_time(input):
     split_input = input.split()
     time = list(map(int, split_input[1].split(':')))
-    if (time[0] not in hours):
+    if time[0] not in hours:
         raise ValueError('Hours should fall between working hours: 9 AM to 5 PM')
-    if (0 <= time[1] > 59):
+    if 0 <= time[1] > 59:
         raise ValueError('Minutes should be between 0 and 60')
     if time[1] == 0:
         time.append(0)
@@ -23,7 +23,7 @@ def get_day(input):
     split_input = input.split()
     day = split_input[0]
     capitalized = day.capitalize()
-    if (capitalized not in days):
+    if capitalized not in days:
         raise ValueError("Day should be a day of the week e.g. 'Monday', 'Tuesday'")
     return capitalized
 
